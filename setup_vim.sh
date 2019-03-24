f44432efaaab112f2a6b9b1a7768dcbe8e444660
aPythonBug <48384164+aPythonBug@users.noreply.github.com>
2019-03-25 01:41:37 +0200

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -3,7 +3,12 @@
 echo "Configurateing the whole system for this vimrc configuration..."
 echo "Installing needed apps to work..."
 
-sudo apt install -y build-essential cmake python3-dev python3-pip python3-venv git vim-tiny
+sudo apt install -y software-properties-common build-essential cmake python3-dev python3-pip python3-venv git vim-tiny
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.6
+
 pip3 install jedi
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
