0c7322e8e35dbb6da10831e1d2d58d55ddb158bd
Dante <apythonbug@gmail.com>
2019-03-24 20:52:34 +0000

    New setup model

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+echo "Configurateing the whole system for this vimrc configuration..."
+echo "Installing needed apps to work..."
+
+sudo apt install build-essential cmake python3-dev git vim-tiny 
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 cp ./vimrc ~/.vim/vimrc
-ln ~/.vimrc ~/.vim/vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
 vim +PluginInstall +qall
 
-echo "dont forget to install / update YoucompleteMe"
-echo "https://github.com/j1z0/dotfiles.git"
-
+# Configurate YouCompleteMe for all langs
+cd $HOME/.vim/bundle/YouCompleteMe
+python3 install.py --all
