8e60b38eb29c3fd6f39d3c3baf2b8a6167cbe2ec
Andi <a.apetroaei@e-tailors.com>
2019-03-25 08:08:26 +0000

    I've mdofified setup

--- setup_vim.sh
+++ setup_vim.sh
@@ -3,11 +3,10 @@
 echo "Configurateing the whole system for this vimrc configuration..."
 echo "Installing needed apps to work..."
 
-sudo apt install -y software-properties-common build-essential cmake python3-dev python3-pip python3-venv git vim-tiny
 
 sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt-get update
-sudo apt-get install python3.6
+sudo apt update
+sudo apt install -y python3.6 python3-dev python3-pip python3-venv software-properties-common build-essential cmake git vim-tiny
 
 pip3 install jedi
 
